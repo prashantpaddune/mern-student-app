a9d236e27ee9abca27dbe27e807df60e3f93a732
prashantpaddune <padadunep@gmail.com>
2021-08-15 12:10:34 +0530

    server: add options to schema

--- server/models/student.js
+++ server/models/student.js
@@ -1,36 +1,18 @@
 const mongoose = require('mongoose');
 
+const options = {
+    type: String,
+    trim: true,
+    required: true
+}
+
 const studentSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    lastName: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    qualification: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    dateOfBirth: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    maritalStatus: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    selectedRadio: {
-        type: String,
-        trim: true,
-        required: true
-    }
+    firstName: options,
+    lastName: options,
+    qualification: options,
+    dateOfBirth: options,
+    maritalStatus: options,
+    selectedRadio: options
 }, { timestamps: true });
 
 module.exports = mongoose.model('Student', studentSchema);
